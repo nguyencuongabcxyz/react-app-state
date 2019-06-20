d7195dd9839fee1d73e50323ed2d4473e51503c8
The Cuong <thecuong.nguyen@sioux.asia>
2019-06-20 23:40:17 +0700

    set state using the way remove constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //we called setstate
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
-
     //React says we have to define render!!
     render() {
         if (this.state.errorMessage && !this.state.lat) {
